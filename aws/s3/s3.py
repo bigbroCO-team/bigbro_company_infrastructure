7c29be34e4b615b79056309a6e5702eef3620124
Jueuunn7 <wndmsrla1234@gmail.com>
2025-01-05 00:45:17 +0900

    Edit s3

--- aws/s3/s3.py
+++ aws/s3/s3.py
@@ -6,7 +6,6 @@ from variable import project_name
 bucket = aws.s3.BucketV2(
     'bigbro-s3',
     bucket='bigbro-s3',
-    acl=aws.s3.CannedAcl.PUBLIC_READ,
     tags={
         'Name': 'bigbro-ec2',
         'Project': project_name
