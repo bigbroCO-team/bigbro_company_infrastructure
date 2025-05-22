deb38aa595f37a6fd3acae4ab9e6fcdf0ce37e3a
Jueuunn7 <wndmsrla1234@gmail.com>
2025-05-22 21:18:17 +0900

    update:

--- __main__.py
+++ __main__.py
@@ -1,10 +1,8 @@
 from aws.vpc import vpc_resource
-from aws.ec2 import ec2_resource
 from aws.s3 import s3_resource
 
 
 aws = {
     'vpc': vpc_resource,
-    'ec2': ec2_resource,
     's3': s3_resource
 }
